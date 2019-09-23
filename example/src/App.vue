54fdcd6cad3ffb9fad05544a1030d6842a314e5a
shadow <wuh131420@foxmail.com>
2019-09-23 15:28:35 +0800

    2019-09-23-[添加测试文件][][shadow]

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,31 +1,27 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <btn-groups key="1a"></btn-groups>
+    <MySearchButtonGroup key="a2"></MySearchButtonGroup>
+    <MySearchGroup v-model="form" key='3c'></MySearchGroup>
   </div>
 </template>
 
 <script>
+import mySearch from 'wh-search-group'
 export default {
   name: 'app',
+  components: {
+    btnGroups: mySearch.MySearchButtonGroup
+  },
   data () {
     return {
+      form: {},
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  created(){
+    console.log(mySearch)
   }
 }
 </script>
@@ -40,21 +36,4 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- example/src/main.js
+++ example/src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import MySearch from 'wh-search-group'
+
+Vue.use(ElementUI)
+Vue.use(MySearch)
 
 new Vue({
   el: '#app',
